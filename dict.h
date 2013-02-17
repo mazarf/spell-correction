3402920e18f09ed820dcc576234912b2b92bb016
Corbin <csgo@ucdavis.edu>
2013-02-16 23:27:46 -0800

    Added constructor and destructor declarations.

---
+++ dict.h
@@ -0,0 +1,19 @@
+//Mazar Farran & Corbin Gomez
+#ifndef DICT_H
+#define DICT_H
+
+class Dict
+{
+public:
+	Dict(string f);
+	~Dict();
+private:
+	/*
+	char **words;
+	char **phrases;		//Will these be necessary once we learn strings?
+	char **sentences;
+	*/
+
+};
+
+#endif
